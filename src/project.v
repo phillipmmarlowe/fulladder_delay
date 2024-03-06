3374d9a27676defb8269754f56e478b8aefa4a3d
Phillip Marlowe <pmarlowe@ucsc.edu>
2024-03-06 14:41:16 -0800

    First push

--- src/project.v
+++ src/project.v
@@ -17,8 +17,66 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  //assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
+  
+  // ui_in[0]  used for Carry in signal
+  // ui_out[0] used for output sum signal
+  // ui_out[1] used for Carry out signal
+  
+  full_adder
+  #()
+  fa_i(
+	.a(1'b1),
+	.b(1'b1),
+	.cin(ui_in[0]),
+	.sum(ui_out[0]),
+	.cout(ui_out[1])
+  );
+  
+endmodule
 
+// Full adder def
+module full_adder(
+    input a, 
+    input b, 
+    input cin, 
+    output sum, 
+    output cout
+);
+    wire sum_half, carry_half1, carry_half2;
+    
+    // First half adder for inputs a and b
+    half_adder ha_i1(
+        .a(a), 
+        .b(b), 
+        .sum(sum_half), 
+        .carry(carry_half1)
+    );
+    
+    // Second half adder for sum of first half adder and cin
+    half_adder ha_i2(
+        .a(sum_half), 
+        .b(cin), 
+        .sum(sum), 
+        .carry(carry_half2)
+    );
+    
+    // OR gate to determine final carry out
+    assign cout = carry_half1 | carry_half2;
+endmodule
+
+
+// Half adder def
+module half_adder(
+    input a, 
+    input b, 
+    output sum, 
+    output carry
+);
+    // Sum is XOR of the two inputs
+    assign sum = a ^ b;
+    // Carry is AND of the two inputs
+    assign carry = a & b;
 endmodule
